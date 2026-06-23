11b2379a0212b04d7e631d4327bf4933aa16fa03
dul-cet <a_moldir@kbtu.kz>
2026-06-23 23:31:29 +0500

    cd

--- esg-news-bot-main/config.py
+++ esg-news-bot-main/config.py
@@ -15,8 +15,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', 'your_telegram_bot_token')
 
 # Хранилизще данных и логов
-DATA_DIR = BASE_DIR / "data"
-LOG_DIR = BASE_DIR / "logs"
+DATA_DIR = Path(os.getenv("DATA_DIR", "/app/data"))
+LOG_DIR = Path(os.getenv("LOG_DIR", "/app/logs"))
 
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 LOG_DIR.mkdir(parents=True, exist_ok=True)
